9e5cc143a35d1c15c44006950f5b05b9103ef67e
SeanBost <97864731+SeanBost@users.noreply.github.com>
2026-02-27 22:40:12 -0500

    basic interactivity added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((c) => c + 1);
+  };
+
   return (
-    <div>
-      <h1>Hello World!!</h1>
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h1>Button Counter</h1>
+      <button onClick={handleClick} style={{ fontSize: "1.25rem", padding: "0.5rem 1rem" }}>
+        Click me
+      </button>
+      <p>Presses: {count}</p>
     </div>
-  )
+  );
 }
